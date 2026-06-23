293c7ae52bddc929d186f67ddd7da32f2e6f2413
Soumik <smik1172@gmail.com>
2026-06-23 13:04:27 +0530

    added: cors for localhost

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
 import products from "./routes/products";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+    "*",
+    cors({
+        origin: ["http://localhost:4321"],
+    }),
+);
 
 app.route("/products", products);
 
